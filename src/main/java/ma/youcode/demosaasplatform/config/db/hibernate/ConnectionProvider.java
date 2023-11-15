6b45784b037e8e9beddcff579ae67e9fe8d173d1
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-11-15 15:57:21 +0100

    new: implement data isolation using hibernate

--- src/main/java/ma/youcode/demosaasplatform/config/db/hibernate/ConnectionProvider.java
+++ src/main/java/ma/youcode/demosaasplatform/config/db/hibernate/ConnectionProvider.java
@@ -1,4 +1,70 @@
 package ma.youcode.demosaasplatform.config.db.hibernate;
 
-public class MultiTenantConnectionProvider {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ma.youcode.demosaasplatform.config.db.context.TenantContext;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ConnectionProvider implements MultiTenantConnectionProvider, HibernatePropertiesCustomizer {
+
+    private final DataSource dataSource;
+
+    @Override
+    public Connection getAnyConnection() throws SQLException {
+        log.info("Connection to {} created",TenantContext.DEFAULT_TENANT_ID);
+        return getConnection(TenantContext.DEFAULT_TENANT_ID);
+    }
+
+    @Override
+    public void releaseAnyConnection(Connection connection) throws SQLException {
+            connection.close();
+    }
+
+    @Override
+    public Connection getConnection(String tenantIdentifier) throws SQLException {
+        var connection = dataSource.getConnection();
+        connection.setSchema(tenantIdentifier);
+        log.info("Connection to tenant {} created", tenantIdentifier);
+        return  connection;
+    }
+
+    @Override
+    public void releaseConnection(String tenantIdentifier, Connection connection) throws SQLException {
+        connection.setSchema(TenantContext.DEFAULT_TENANT_ID);
+        connection.close();
+    }
+
+    @Override
+    public boolean supportsAggressiveRelease() {
+        return true;
+    }
+
+    @Override
+    public boolean isUnwrappableAs(Class<?> unwrapType) {
+        return false;
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> unwrapType) {
+        return null;
+    }
+
+    @Override
+    public void customize(Map<String, Object> hibernateProperties) {
+            hibernateProperties.put(AvailableSettings.MULTI_TENANT_CONNECTION_PROVIDER, this);
+            hibernateProperties.remove(AvailableSettings.DEFAULT_SCHEMA);
+
+    }
 }
--- src/main/java/ma/youcode/demosaasplatform/config/db/hibernate/TenantIdentifierResolver.java
+++ src/main/java/ma/youcode/demosaasplatform/config/db/hibernate/TenantIdentifierResolver.java
@@ -1,2 +1,30 @@
-package ma.youcode.demosaasplatform.config.db.hibernate;public class TenantIdentifierResolver {
+package ma.youcode.demosaasplatform.config.db.hibernate;
+
+import ma.youcode.demosaasplatform.config.db.context.TenantContext;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Objects;
+
+// HibernatePropertiesCustomizer is used explicitly configure Hibernate to use this resolver
+// Hibernate will not allow spring to define CurrentTenantIdentifierResolver
+@Component
+public class TenantIdentifierResolver implements CurrentTenantIdentifierResolver, HibernatePropertiesCustomizer {
+    @Override
+    public String resolveCurrentTenantIdentifier() {
+        return Objects.requireNonNullElse(TenantContext.getTenantId(), "default_001");
+    }
+
+    @Override
+    public boolean validateExistingCurrentSessions() {
+        return false;
+    }
+
+    @Override
+    public void customize(Map<String, Object> hibernateProperties) {
+            hibernateProperties.put(AvailableSettings.MULTI_TENANT_IDENTIFIER_RESOLVER, this);
+    }
 }
