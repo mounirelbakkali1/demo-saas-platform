ce4502daf788678853a7cb5f92c2ce11b046d84f
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-11-15 15:57:58 +0100

    update: using postgresql container insteadÃ

--- src/test/java/ma/youcode/demosaasplatform/DemoSaasPlatformApplicationTests.java
+++ src/test/java/ma/youcode/demosaasplatform/DemoSaasPlatformApplicationTests.java
@@ -2,8 +2,10 @@ package ma.youcode.demosaasplatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestDemoSaasPlatformApplication.class)
 class DemoSaasPlatformApplicationTests {
 
 	@Test
--- src/test/java/ma/youcode/demosaasplatform/TestDemoSaasPlatformApplication.java
+++ src/test/java/ma/youcode/demosaasplatform/TestDemoSaasPlatformApplication.java
@@ -2,12 +2,26 @@ package ma.youcode.demosaasplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestDemoSaasPlatformApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(DemoSaasPlatformApplication::main).with(TestDemoSaasPlatformApplication.class).run(args);
+		SpringApplication
+				.from(DemoSaasPlatformApplication::main)
+				.with(TestDemoSaasPlatformApplication.class)
+				.run(args);
+	}
+
+
+
+	@Bean
+	@ServiceConnection
+	public PostgreSQLContainer<?> mysqlContainer() {
+		return new PostgreSQLContainer<>("postgres:15");
 	}
 
 }
