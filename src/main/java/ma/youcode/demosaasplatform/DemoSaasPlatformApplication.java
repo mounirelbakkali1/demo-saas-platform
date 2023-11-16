c77db060b76d877448618d22ab7aaaa2a7af9599
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-11-16 16:08:04 +0100

    fix: imports not found

--- src/main/java/ma/youcode/demosaasplatform/DemoSaasPlatformApplication.java
+++ src/main/java/ma/youcode/demosaasplatform/DemoSaasPlatformApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
